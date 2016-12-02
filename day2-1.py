f9b8ce5bbee4afebcea3d5b8b5f422b9a7c61d31
James Bannerman <james.bannerman@indexexchange.com>
2016-12-02 14:40:56 -0500

    working solution for day 2

---
+++ day2-1.py
@@ -0,0 +1,15 @@
+f = open('input.txt','r')
+sum = 0
+for i in f.readlines():
+	l, w, h = i.split("x")
+	l = int(l)
+	w = int(w)
+	h = int(h)
+	side1 = l * w
+	side2 = w * h
+	side3 = h * l
+	smallest = min(side1, side2, side3)
+	area = 2 * side1 + 2 * side2 + 2 * side3 + smallest
+	sum = sum + area
+print "total wrapping paper needed is " + str(sum)
+		
---
+++ day2-2.py
@@ -0,0 +1,16 @@
+
+
+f = open('input.txt','r')
+sum = 0
+for i in f.readlines():
+	l, w, h = i.split("x")
+	l = int(l)
+	w = int(w)
+	h = int(h)
+	smallest = min(l, w, h)
+	middle = sorted([l,w,h])[1]
+	length = 2 * smallest + 2 * middle
+	bow = l * w * h
+	sum = sum + length + bow
+print "total ribbon needed is " + str(sum)
+		
