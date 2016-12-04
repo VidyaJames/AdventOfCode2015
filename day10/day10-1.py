ce00dbcf630c7465c7c4aa77f7f8df2c6fa318e2
James Bannerman <james.bannerman@indexexchange.com>
2016-12-03 23:55:02 -0500

    Working solution for day 10

---
+++ day10/day10-1.py
@@ -0,0 +1,19 @@
+start = "1113222113"
+iterations = 40
+i = 0
+while i < iterations:
+	newString = ""
+	while start != "":
+		first = start[0]
+		counter = 0
+		for j in start:
+			if j == first:
+				counter = counter + 1
+			else:
+				break
+		start = start[counter:]
+		newString = newString + str(counter)
+		newString = newString + first
+	start = newString
+	i = i + 1
+print "The length of the string is " + len(start) + "."
